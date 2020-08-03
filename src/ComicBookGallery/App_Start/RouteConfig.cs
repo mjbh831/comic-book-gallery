6edac8760282469a7ef1fc7866abb21b89001773
mjbh831 <56654912+mjbh831@users.noreply.github.com>
2020-08-03 15:07:16 -0500

    Added ComicBooksController

--- src/ComicBookGallery/App_Start/RouteConfig.cs
+++ src/ComicBookGallery/App_Start/RouteConfig.cs
@@ -16,8 +16,7 @@ namespace ComicBookGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                //changes for committ
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }           
             );
         }
     }
